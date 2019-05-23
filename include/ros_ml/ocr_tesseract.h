2b4ac78ddee122fcb0ad8d9dc22f598993b4c4c9
Nguyen Quang <nqoptik@gmail.com>
2019-05-23 10:30:01 +0800

    Apply orc tesseract for yolo detection output

--- include/ros_ml/ocr_tesseract.h
+++ include/ros_ml/ocr_tesseract.h
@@ -12,10 +12,14 @@
 #define _OCR_TESSERACT_H_
 
 #include <iostream>
+#include <string>
+#include <sstream>
 
 #include <ros/ros.h>
+#include <std_msgs/String.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/text.hpp>
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
@@ -33,9 +37,13 @@ class OCRTesseract {
     ros::NodeHandle node;                                             //!< @brief The ros node handle. @since 0.0.1
     message_filters::Subscriber<sensor_msgs::Image> yolo_image_sub;   //!< @brief The YOLO detection image. @since 0.0.1
     message_filters::Subscriber<ros_ml::YoloResult> yolo_result_sub;  //!< @brief The YOLO detection result. @since 0.0.1
+
     typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, ros_ml::YoloResult> MySyncPolicy;
     typedef message_filters::Synchronizer<MySyncPolicy> Sync;
     boost::shared_ptr<Sync> sync;  //!< @brief The Synchronizer shared ponter. @since 0.0.1
+
+    ros::Publisher ocr_result_pub;                  //!< @brief The OCRTesseract result publisher. @since 0.0.1
+    cv::Ptr<cv::text::OCRTesseract> ocr_tesseract;  //!< @brief The OCRTesseract object. @since 0.0.1
 
    public:
     /**
--- src/ocr_tesseract.cpp
+++ src/ocr_tesseract.cpp
@@ -16,6 +16,8 @@ OCRTesseract::OCRTesseract(ros::NodeHandle node) {
     yolo_result_sub.subscribe(node, "/yolo_detection_result", 1);
     sync.reset(new Sync(MySyncPolicy(10), yolo_image_sub, yolo_result_sub));
     sync->registerCallback(boost::bind(&OCRTesseract::callback, this, _1, _2));
+    ocr_result_pub = node.advertise<std_msgs::String>("ocr_tesseract_result_pub", 1);
+    ocr_tesseract = cv::text::OCRTesseract::create(NULL, "eng", "-0123456789", cv::text::OEM_DEFAULT, cv::text::PSM_SINGLE_BLOCK);
 }
 
 OCRTesseract::~OCRTesseract() {
@@ -28,7 +30,58 @@ void OCRTesseract::callback(const sensor_msgs::ImageConstPtr& img_msg, const ros
     } catch (cv_bridge::Exception& e) {
         ROS_ERROR("Could not convert from '%s' to 'mono8'.", img_msg->encoding.c_str());
     }
-    cv::imshow("image", frame);
-    std::cout << result_msg->giant_locations.size() << std::endl;
-    cv::waitKey(1);
+
+    std::stringstream ss_pub;
+    // For each location got from YOLO detection
+    for (int i_l = 0; i_l < result_msg->giant_locations.size(); i_l++) {
+        std::string output;
+        std::vector<cv::Rect> boxes;
+        std::vector<std::string> words;
+        std::vector<float> confidences;
+        cv::Rect rect(cv::Point(result_msg->giant_locations[i_l].tl.x, result_msg->giant_locations[i_l].tl.y),
+                      cv::Point(result_msg->giant_locations[i_l].br.x, result_msg->giant_locations[i_l].br.y));
+        cv::Mat cropped_image = frame(rect);
+
+        // Run OCRTesseract
+        ocr_tesseract->run(cropped_image, output, &boxes, &words, &confidences, cv::text::OCR_LEVEL_WORD);
+
+        // Filter the ORCTesseract output to get the location
+        for (int w = 0; w < words.size(); w++) {
+            std::string word = words[w];
+            if (word.length() >= 8) {
+                // Find the two positions of '-' letters
+                int pos_1 = 0, pos_2 = 0;
+                for (int i = 0; i < word.length(); i++) {
+                    if (word.c_str()[i] == '-') {
+                        pos_1 = i;
+                        for (int j = i + 1; j < word.length(); j++) {
+                            if (word.c_str()[j] == '-') {
+                                pos_2 = j;
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+
+                // If the two positions of '-' letters are separated by an one digit number
+                // and there are more than 4 digits before the first position
+                if (pos_2 - pos_1 == 2 && pos_1 >= 4) {
+                    std::string result = word.substr(pos_1 - 4, 8);
+                    bool good_result = true;
+                    for (int i = 0; i < word.length(); i++) {
+                        if (result.c_str()[i] == ' ') {
+                            good_result = false;
+                        }
+                    }
+                    if (good_result) {
+                        ss_pub << word.substr(pos_1 - 4, 8);
+                    }
+                }
+            }
+        }
+    }
+    std_msgs::String msg_string;
+    msg_string.data = ss_pub.str();
+    ocr_result_pub.publish(msg_string);
 }
